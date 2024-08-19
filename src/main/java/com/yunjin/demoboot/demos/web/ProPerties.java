f8ed2e682723b71c36310654b897f35e8b7d935f
yangj280 <yangj280@chinaunicom.cn>
2024-08-19 22:28:22 +0800

    init

---
+++ src/main/java/com/yunjin/demoboot/demos/web/ProPerties.java
@@ -0,0 +1,8 @@
+package com.yunjin.demoboot.demos.web;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class ProPerties {
+}
